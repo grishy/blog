6b0f7324c774b3996491d9c42950436d18c3301b
Grishy <mailgrishy@gmail.com>
2019-06-15 11:53:42 +0300

    Favicon

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,9 +2,92 @@ import React from "react"
 import { Link } from "gatsby"
 import style from "./menu.module.scss"
 
-export default () => (
-  <header className={style.header}>
-     <Link to="/" className={style.header_title}>Grishy</Link>
-  </header>
-)
+const Bars = () => {
+  return (
+    <svg viewBox="0 0 24 28">
+      <path d="M24 21v2c0 .5-.5 1-1 1H1c-.5 0-1-.5-1-1v-2c0-.5.5-1 1-1h22c.5 0 1 .5 1 1zm0-8v2c0 .5-.5 1-1 1H1c-.5 0-1-.5-1-1v-2c0-.5.5-1 1-1h22c.5 0 1 .5 1 1zm0-8v2c0 .5-.5 1-1 1H1c-.5 0-1-.5-1-1V5c0-.5.5-1 1-1h22c.5 0 1 .5 1 1z"></path>
+    </svg>
+  )
+}
+const Close = () => {
+  return (
+    <svg viewBox="0 0 24 28">
+      <path d="M20.3 20.7c0 0.4-0.2 0.8-0.4 1.1l-2.1 2.1c-0.3 0.3-0.7 0.4-1.1 0.4s-0.8-0.2-1.1-0.4l-4.6-4.6-4.6 4.6c-0.3 0.3-0.7 0.4-1.1 0.4s-0.8-0.2-1.1-0.4l-2.1-2.1c-0.3-0.3-0.4-0.7-0.4-1.1s0.2-0.8 0.4-1.1l4.6-4.6-4.6-4.6c-0.3-0.3-0.4-0.7-0.4-1.1s0.2-0.8 0.4-1.1l2.1-2.1c0.3-0.3 0.7-0.4 1.1-0.4s0.8 0.2 1.1 0.4l4.6 4.6 4.6-4.6c0.3-0.3 0.7-0.4 1.1-0.4s0.8 0.2 1.1 0.4l2.1 2.1c0.3 0.3 0.4 0.7 0.4 1.1s-0.2 0.8-0.4 1.1l-4.6 4.6 4.6 4.6c0.3 0.3 0.4 0.7 0.4 1.1z"></path>
+    </svg>
+  )
+}
 
+class Player extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      playing: false,
+    }
+  }
+
+  handlePlayerClick = () => {
+    if (!this.state.playing) {
+      this.setState({ playing: true })
+    } else {
+      this.setState({ playing: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className="player" onClick={this.handlePlayerClick}>
+        {this.state.playing ? <Bars /> : <Close />}
+      </div>
+    )
+  }
+}
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      elems: [
+        {
+          to: "/",
+          title: "Главная",
+        },
+        {
+          to: "/treasures",
+          title: "Сокровища",
+        },
+        {
+          to: "/about",
+          title: "Обо мне",
+        },
+        {
+          to: "/archive",
+          title: "Архив",
+        },
+      ],
+    }
+  }
+
+  render() {
+    const listItems = this.state.elems.map(el => (
+      <li>
+        <Link title={el.title} to={el.to}>
+          {el.title}
+        </Link>
+      </li>
+    ))
+
+    return (
+      <header className={style.header}>
+        <Link to="/" className={style.header_title}>
+          Grishy
+        </Link>
+
+        <ul className={style.header_navigation}>{listItems}</ul>
+
+        <Player />
+      </header>
+    )
+  }
+}
+
+export default Menu
---
+++ static/README.md
@@ -0,0 +1,13 @@
+<p>
+  <h2 align="center">
+    <a href="http://grishy.ru/">
+      <img width="220" src='https://cdn.dribbble.com/users/60266/screenshots/1560009/gifdribbble_cc_aerolab.gif'/><br>
+      <i>Grishy.ru</i>
+    </a>
+    - Блог о программировании и...
+  </h2>
+</p>
+
+### Только хранилище, основной код находится [тут](https://github.com/Grishy/blog)
+
+:anchor: [Grishy](https://github.com/Grishy)
